734d73e927f5f0f12b4e96825f27ff9b16797400
Maksim <tsarykmk@gmail.com>
2020-04-04 17:51:49 +0300

    Fix error messages

--- src/GodelTech.Data.EntityFrameworkCore/Repository.cs
+++ src/GodelTech.Data.EntityFrameworkCore/Repository.cs
@@ -185,13 +185,13 @@ namespace GodelTech.Data.EntityFrameworkCore
         protected virtual IQueryable<TEntity> PagedResultQuery(QueryParameters<TEntity, TType> queryParameters)
         {
             if (queryParameters == null)
-                throw new ArgumentNullException(nameof(queryParameters), "Query Parameters can't be null.");
+                throw new ArgumentNullException(nameof(queryParameters));
 
             if (queryParameters.Page == null)
-                throw new ArgumentNullException(nameof(queryParameters.Page), "Query Parameters Page can't be null.");
+                throw new ArgumentNullException(nameof(queryParameters.Page));
 
             if (!queryParameters.Page.IsValid)
-                throw new ArgumentException("Query Parameters Page is not valid.", nameof(queryParameters.Page));
+                throw new ArgumentException("Query Parameters Page is not valid.");
 
             return Query(queryParameters);
         }
