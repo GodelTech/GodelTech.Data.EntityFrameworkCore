26580d813b9c4587439826dfeb29726754810a7d
Maksim <tsarykmk@gmail.com>
2020-04-06 16:30:34 +0300

    Replace database app with console app

---
+++ demo/Database/src/GodelTech.Demo.Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GodelTech.Demo.Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
