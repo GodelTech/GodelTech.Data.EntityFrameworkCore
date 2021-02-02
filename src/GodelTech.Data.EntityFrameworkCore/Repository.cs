5ec28b9350139c432008c4f9ef5cc48385a71805
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-02-02 13:38:13 +0300

    solve issue after change GodelTech.Data.Extensions namespace

--- src/GodelTech.Data.EntityFrameworkCore/Repository.cs
+++ src/GodelTech.Data.EntityFrameworkCore/Repository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GodelTech.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace GodelTech.Data.EntityFrameworkCore
