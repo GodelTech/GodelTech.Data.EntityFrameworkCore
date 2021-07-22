5da12feb93703404bfc7aef72f25875bd1200a46
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-07-22 23:06:02 +0300

    add Count unit tests

---
+++ test/GodelTech.Data.EntityFrameworkCore.Tests/RepositoryAsyncTests.Count.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using GodelTech.Data.EntityFrameworkCore.Tests.Fakes;
+using MockQueryable.Moq;
+using Moq;
+using Xunit;
+
+namespace GodelTech.Data.EntityFrameworkCore.Tests
+{
+    public partial class RepositoryAsyncTests
+    {
+        [Theory]
+        [MemberData(nameof(RepositoryTests.QueryMemberData), MemberType = typeof(RepositoryTests))]
+        public async Task CountAsync_Success<TKey>(
+            TKey defaultKey,
+            QueryParameters<FakeEntity<TKey>, TKey> queryParameters,
+            Collection<FakeEntity<TKey>> entities,
+            IQueryable<FakeEntity<TKey>> queryableEntities,
+            int filteredEntitiesCount)
+        {
+            // Arrange
+            var query = entities.AsQueryable();
+
+            if (queryParameters?.Filter?.Expression != null)
+            {
+                query = query.Where(queryParameters.Filter.Expression);
+            }
+
+            var expectedResult = query.Count();
+
+            var repository = GetRepository<FakeEntity<TKey>, TKey>(entities);
+
+            // Act
+            var result = await repository.CountAsync(queryParameters);
+
+            // Assert
+            Assert.NotNull(defaultKey);
+            Assert.Equal(filteredEntitiesCount, queryableEntities.ToList().Count);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
