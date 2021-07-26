64febf960478e42b83e4fa4ce7eeb1d86eca42a3
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-07-26 17:11:31 +0300

    add tests for protected methods of repository

---
+++ test/GodelTech.Data.EntityFrameworkCore.Tests/Fakes/FakeRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace GodelTech.Data.EntityFrameworkCore.Tests.Fakes
+{
+    public class FakeRepository<TEntity, TKey> : Repository<TEntity, TKey>
+        where TEntity : class, IEntity<TKey>
+    {
+        public FakeRepository(DbContext dbContext, IDataMapper dataMapper)
+            : base(dbContext, dataMapper)
+        {
+
+        }
+
+        public IQueryable<TEntity> ExposedQuery(QueryParameters<TEntity, TKey> queryParameters = null)
+        {
+            return Query(queryParameters);
+        }
+
+        public IQueryable<TModel> ExposedQuery<TModel>(QueryParameters<TEntity, TKey> queryParameters = null)
+        {
+            return Query<TModel>(queryParameters);
+        }
+
+        public IQueryable<TEntity> ExposedPagedResultQuery(QueryParameters<TEntity, TKey> queryParameters)
+        {
+            return PagedResultQuery(queryParameters);
+        }
+
+        public IQueryable<TModel> ExposedPagedResultQuery<TModel>(QueryParameters<TEntity, TKey> queryParameters)
+        {
+            return PagedResultQuery<TModel>(queryParameters);
+        }
+
+        public IQueryable<TEntity> ExposedCountQuery(QueryParameters<TEntity, TKey> queryParameters = null)
+        {
+            return CountQuery(queryParameters);
+        }
+
+        // not possible to mock EntityEntry<TEntity>, so no usage in unit tests
+        public void ExposedMarkAsModified(TEntity entity)
+        {
+            MarkAsModified(entity);
+        }
+
+        // not possible to mock EntityEntry<TEntity>, so no usage in unit tests
+        public bool ExposedIsDetached(TEntity entity)
+        {
+            return IsDetached(entity);
+        }
+    }
+}
--- test/GodelTech.Data.EntityFrameworkCore.Tests/RepositoryTests.cs
+++ test/GodelTech.Data.EntityFrameworkCore.Tests/RepositoryTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using GodelTech.Data.EntityFrameworkCore.Tests.Fakes;
 using Microsoft.EntityFrameworkCore;
 using Moq;
+using Xunit;
 
 namespace GodelTech.Data.EntityFrameworkCore.Tests
 {
@@ -299,6 +300,344 @@ namespace GodelTech.Data.EntityFrameworkCore.Tests
                 // string
             };
 
+        [Theory]
+        [MemberData(nameof(QueryMemberData))]
+        public void Query_Success<TKey>(
+            TKey defaultKey,
+            QueryParameters<FakeEntity<TKey>, TKey> queryParameters,
+            Collection<FakeEntity<TKey>> entities,
+            IQueryable<FakeEntity<TKey>> queryableEntities,
+            int filteredEntitiesCount)
+        {
+            // Arrange
+            var expectedResult = queryableEntities.ToList();
+
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(entities);
+
+            // Act
+            var result = repository.ExposedQuery(queryParameters).ToList();
+
+            // Assert
+            Assert.NotNull(defaultKey);
+            Assert.Equal(filteredEntitiesCount, queryableEntities.ToList().Count);
+            Assert.Equal(expectedResult, result, new FakeEntityEqualityComparer<TKey>());
+        }
+
+        [Theory]
+        [MemberData(nameof(QueryMemberData))]
+        public void QueryModel_Success<TKey>(
+            TKey defaultKey,
+            QueryParameters<FakeEntity<TKey>, TKey> queryParameters,
+            Collection<FakeEntity<TKey>> entities,
+            IQueryable<FakeEntity<TKey>> queryableEntities,
+            int filteredEntitiesCount)
+        {
+            // Arrange
+            var expectedResult = queryableEntities
+                .Select(
+                    x => new FakeModel<TKey>
+                    {
+                        Id = x.Id,
+                        Name = x.Name
+                    }
+                )
+                .ToList();
+
+            _mockDataMapper
+                .Setup(
+                    x => x.Map<FakeModel<TKey>>(
+                        It.Is<IQueryable<FakeEntity<TKey>>>(
+                            y => !y
+                                .Except(queryableEntities)
+                                .Any()
+                        )
+                    )
+                )
+                .Returns(
+                    queryableEntities
+                        .Select(
+                            x => new FakeModel<TKey>
+                            {
+                                Id = x.Id,
+                                Name = x.Name
+                            }
+                        )
+                );
+
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(entities);
+
+            // Act
+            var result = repository.ExposedQuery<FakeModel<TKey>>(queryParameters).ToList();
+
+            // Assert
+            _mockDataMapper
+                .Verify(
+                    x => x.Map<FakeModel<TKey>>(
+                        It.Is<IQueryable<FakeEntity<TKey>>>(
+                            y => !y
+                                .Except(queryableEntities)
+                                .Any()
+                        )
+                    ),
+                    Times.Once
+                );
+
+            Assert.NotNull(defaultKey);
+            Assert.Equal(filteredEntitiesCount, queryableEntities.ToList().Count);
+            Assert.Equal(expectedResult, result, new FakeModelEqualityComparer<TKey>());
+        }
+
+        [Theory]
+        [MemberData(nameof(UnitOfWorkTests.TypesMemberData), MemberType = typeof(UnitOfWorkTests))]
+        public void PagedResultQuery_WhenQueryParametersIsNull_ThrowsArgumentNullException<TKey>(
+            TKey defaultKey)
+        {
+            // Arrange
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(new List<FakeEntity<TKey>>());
+
+            // Act & Assert
+            Assert.NotNull(defaultKey);
+
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => repository.ExposedPagedResultQuery(null)
+            );
+            Assert.Equal("queryParameters", exception.ParamName);
+        }
+
+        [Theory]
+        [MemberData(nameof(UnitOfWorkTests.TypesMemberData), MemberType = typeof(UnitOfWorkTests))]
+        public void PagedResultQuery_WhenQueryParametersPageIsNull_ThrowsArgumentException<TKey>(
+            TKey defaultKey)
+        {
+            // Arrange
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(new List<FakeEntity<TKey>>());
+
+            // Act & Assert
+            Assert.NotNull(defaultKey);
+
+            var exception = Assert.Throws<ArgumentException>(
+                () => repository.ExposedPagedResultQuery(
+                    new QueryParameters<FakeEntity<TKey>, TKey>
+                    {
+                        Page = null
+                    }
+                )
+            );
+            Assert.Equal("Page can't be null. (Parameter 'queryParameters')", exception.Message);
+            Assert.Equal("queryParameters", exception.ParamName);
+        }
+
+        [Theory]
+        [MemberData(nameof(UnitOfWorkTests.TypesMemberData), MemberType = typeof(UnitOfWorkTests))]
+        public void PagedResultQuery_WhenQueryParametersPageIsNotValid_ThrowsArgumentException<TKey>(
+            TKey defaultKey)
+        {
+            // Arrange
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(new List<FakeEntity<TKey>>());
+
+            // Act & Assert
+            Assert.NotNull(defaultKey);
+
+            var exception = Assert.Throws<ArgumentException>(
+                () => repository.ExposedPagedResultQuery(
+                    new QueryParameters<FakeEntity<TKey>, TKey>
+                    {
+                        Page = new PageRule
+                        {
+                            Index = -1,
+                            Size = -1
+                        }
+                    }
+                )
+            );
+            Assert.Equal("Page is not valid. (Parameter 'queryParameters')", exception.Message);
+            Assert.Equal("queryParameters", exception.ParamName);
+        }
+
+        [Theory]
+        [MemberData(nameof(PagedResultQueryMemberData))]
+        public void PagedResultQuery_Success<TKey>(
+            TKey defaultKey,
+            QueryParameters<FakeEntity<TKey>, TKey> queryParameters,
+            Collection<FakeEntity<TKey>> entities,
+            IQueryable<FakeEntity<TKey>> queryableEntities,
+            int filteredEntitiesCount)
+        {
+            // Arrange
+            var expectedResult = queryableEntities.ToList();
+
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(entities);
+
+            // Act
+            var result = repository.ExposedPagedResultQuery(queryParameters).ToList();
+
+            // Assert
+            Assert.NotNull(defaultKey);
+            Assert.Equal(filteredEntitiesCount, queryableEntities.ToList().Count);
+            Assert.Equal(expectedResult, result, new FakeEntityEqualityComparer<TKey>());
+        }
+
+        [Theory]
+        [MemberData(nameof(UnitOfWorkTests.TypesMemberData), MemberType = typeof(UnitOfWorkTests))]
+        public void PagedResultQueryModel_WhenQueryParametersIsNull_ThrowsArgumentNullException<TKey>(
+            TKey defaultKey)
+        {
+            // Arrange
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(new List<FakeEntity<TKey>>());
+
+            // Act & Assert
+            Assert.NotNull(defaultKey);
+
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => repository.ExposedPagedResultQuery<FakeModel<TKey>>(null)
+            );
+            Assert.Equal("queryParameters", exception.ParamName);
+        }
+
+        [Theory]
+        [MemberData(nameof(UnitOfWorkTests.TypesMemberData), MemberType = typeof(UnitOfWorkTests))]
+        public void PagedResultQueryModel_WhenQueryParametersPageIsNull_ThrowsArgumentException<TKey>(
+            TKey defaultKey)
+        {
+            // Arrange
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(new List<FakeEntity<TKey>>());
+
+            // Act & Assert
+            Assert.NotNull(defaultKey);
+
+            var exception = Assert.Throws<ArgumentException>(
+                () => repository.ExposedPagedResultQuery<FakeModel<TKey>>(
+                    new QueryParameters<FakeEntity<TKey>, TKey>
+                    {
+                        Page = null
+                    }
+                )
+            );
+            Assert.Equal("Page can't be null. (Parameter 'queryParameters')", exception.Message);
+            Assert.Equal("queryParameters", exception.ParamName);
+        }
+
+        [Theory]
+        [MemberData(nameof(UnitOfWorkTests.TypesMemberData), MemberType = typeof(UnitOfWorkTests))]
+        public void PagedResultQueryModel_WhenQueryParametersPageIsNotValid_ThrowsArgumentException<TKey>(
+            TKey defaultKey)
+        {
+            // Arrange
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(new List<FakeEntity<TKey>>());
+
+            // Act & Assert
+            Assert.NotNull(defaultKey);
+
+            var exception = Assert.Throws<ArgumentException>(
+                () => repository.ExposedPagedResultQuery<FakeModel<TKey>>(
+                    new QueryParameters<FakeEntity<TKey>, TKey>
+                    {
+                        Page = new PageRule
+                        {
+                            Index = -1,
+                            Size = -1
+                        }
+                    }
+                )
+            );
+            Assert.Equal("Page is not valid. (Parameter 'queryParameters')", exception.Message);
+            Assert.Equal("queryParameters", exception.ParamName);
+        }
+
+        [Theory]
+        [MemberData(nameof(PagedResultQueryMemberData))]
+        public void PagedResultQueryModel_Success<TKey>(
+            TKey defaultKey,
+            QueryParameters<FakeEntity<TKey>, TKey> queryParameters,
+            Collection<FakeEntity<TKey>> entities,
+            IQueryable<FakeEntity<TKey>> queryableEntities,
+            int filteredEntitiesCount)
+        {
+            // Arrange
+            var expectedResult = queryableEntities
+                .Select(
+                    x => new FakeModel<TKey>
+                    {
+                        Id = x.Id,
+                        Name = x.Name
+                    }
+                )
+                .ToList();
+
+            _mockDataMapper
+                .Setup(
+                    x => x.Map<FakeModel<TKey>>(
+                        It.Is<IQueryable<FakeEntity<TKey>>>(
+                            y => !y
+                                .Except(queryableEntities)
+                                .Any()
+                        )
+                    )
+                )
+                .Returns(
+                    queryableEntities
+                        .Select(
+                            x => new FakeModel<TKey>
+                            {
+                                Id = x.Id,
+                                Name = x.Name
+                            }
+                        )
+                );
+
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(entities);
+
+            // Act
+            var result = repository.ExposedPagedResultQuery<FakeModel<TKey>>(queryParameters);
+
+            // Assert
+            _mockDataMapper
+                .Verify(
+                    x => x.Map<FakeModel<TKey>>(
+                        It.Is<IQueryable<FakeEntity<TKey>>>(
+                            y => !y
+                                .Except(queryableEntities)
+                                .Any()
+                        )
+                    ),
+                    Times.Once
+                );
+
+            Assert.NotNull(defaultKey);
+            Assert.Equal(filteredEntitiesCount, queryableEntities.ToList().Count);
+            Assert.Equal(expectedResult, result, new FakeModelEqualityComparer<TKey>());
+        }
+
+        [Theory]
+        [MemberData(nameof(QueryMemberData))]
+        public void CountQuery_Success<TKey>(
+            TKey defaultKey,
+            QueryParameters<FakeEntity<TKey>, TKey> queryParameters,
+            Collection<FakeEntity<TKey>> entities,
+            IQueryable<FakeEntity<TKey>> queryableEntities,
+            int filteredEntitiesCount)
+        {
+            // Arrange
+            var query = entities.AsQueryable();
+
+            if (queryParameters?.Filter?.Expression != null)
+            {
+                query = query.Where(queryParameters.Filter.Expression);
+            }
+
+            var expectedResult = query.Count();
+
+            var repository = (FakeRepository<FakeEntity<TKey>, TKey>) GetRepository<FakeEntity<TKey>, TKey>(entities);
+
+            // Act
+            var result = repository.ExposedCountQuery(queryParameters).Count();
+
+            // Assert
+            Assert.NotNull(defaultKey);
+            Assert.Equal(filteredEntitiesCount, queryableEntities.ToList().Count);
+            Assert.Equal(expectedResult, result);
+        }
+
         private Repository<TEntity, TKey> GetRepository<TEntity, TKey>(ICollection<TEntity> entities)
             where TEntity : class, IEntity<TKey>
         {
@@ -313,7 +652,7 @@ namespace GodelTech.Data.EntityFrameworkCore.Tests
                 .Setup(x => x.Set<TEntity>())
                 .Returns(() => mockDbSet.Object);
 
-            return new Repository<TEntity, TKey>(
+            return new FakeRepository<TEntity, TKey>(
                 _mockDbContext.Object,
                 _mockDataMapper.Object
             );
