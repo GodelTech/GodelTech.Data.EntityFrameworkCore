0bde8be8e73dfcaecd057675b89ebd707f0ae0e3
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-08-02 13:39:39 +0300

    add repository insert async tests

---
+++ test/GodelTech.Data.EntityFrameworkCore.IntegrationTests/RepositoryAsyncTests.Insert.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using GodelTech.Data.EntityFrameworkCore.IntegrationTests.Fakes;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace GodelTech.Data.EntityFrameworkCore.IntegrationTests
+{
+    public sealed partial class RepositoryAsyncTests : IDisposable
+    {
+        [Theory]
+        [MemberData(nameof(RepositoryTests.InsertMemberData), MemberType = typeof(RepositoryTests))]
+        public async Task InsertAsync_Success<TKey>(
+            TKey defaultKey,
+            FakeEntity<TKey> entity,
+            Collection<FakeEntity<TKey>> expectedEntities)
+        {
+            // Arrange & Act
+            var result = await _unitOfWork
+                .GetFakeTypeEntityRepository<TKey>()
+                .InsertAsync(entity);
+
+            await _unitOfWork.CommitAsync();
+
+            // Assert
+            Assert.NotNull(defaultKey);
+
+            var dbContextEntityResult = await DbContext
+                .Set<FakeEntity<TKey>>()
+                .SingleAsync(x => x.Id.Equals(entity.Id));
+
+            Assert.Equal(entity, result);
+            Assert.Equal(dbContextEntityResult, result, new FakeEntityEqualityComparer<TKey>());
+
+            var dbContextResult = await DbContext
+                .Set<FakeEntity<TKey>>()
+                .ToListAsync();
+
+            Assert.Equal(expectedEntities, dbContextResult, new FakeEntityEqualityComparer<TKey>());
+        }
+
+        [Theory]
+        [MemberData(nameof(RepositoryTests.InsertListMemberData), MemberType = typeof(RepositoryTests))]
+        public async Task InsertListAsync_Success<TKey>(
+            TKey defaultKey,
+            Collection<FakeEntity<TKey>> entities,
+            Collection<FakeEntity<TKey>> expectedEntities)
+        {
+            // Arrange & Act
+            await _unitOfWork
+                .GetFakeTypeEntityRepository<TKey>()
+                .InsertAsync(entities);
+
+            await _unitOfWork.CommitAsync();
+
+            // Assert
+            Assert.NotNull(defaultKey);
+
+            var dbContextResult = await DbContext
+                .Set<FakeEntity<TKey>>()
+                .ToListAsync();
+
+            Assert.Equal(expectedEntities, dbContextResult, new FakeEntityEqualityComparer<TKey>());
+        }
+    }
+}
--- test/GodelTech.Data.EntityFrameworkCore.IntegrationTests/RepositoryAsyncTests.cs
+++ test/GodelTech.Data.EntityFrameworkCore.IntegrationTests/RepositoryAsyncTests.cs
@@ -1,183 +1,45 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Aurochses.Xunit;
-//using GodelTech.Data.EntityFrameworkCore.IntegrationTests.Fakes;
-//using Microsoft.EntityFrameworkCore;
-//using Xunit;
+﻿using System;
+using AutoMapper;
+using AutoMapper.Configuration;
+using GodelTech.Data.EntityFrameworkCore.IntegrationTests.Fakes;
+using Microsoft.EntityFrameworkCore;
 
-//namespace GodelTech.Data.EntityFrameworkCore.IntegrationTests
-//{
-//    public partial class RepositoryTests
-//    {
-//        #region Get
+namespace GodelTech.Data.EntityFrameworkCore.IntegrationTests
+{
+    public sealed partial class RepositoryAsyncTests : IDisposable
+    {
+        private readonly FakeUnitOfWork _unitOfWork;
 
-//        [Theory]
-//        [MemberData(nameof(QueryParametersMemberData))]
-//        public async Task GetAsync_Success(QueryParameters<FakeEntity, int> queryParameters, IQueryable<FakeEntity> queryable, IQueryable<FakeEntity> countQueryable)
-//        {
-//            ValidateQueryParametersMemberData(queryable, countQueryable);
+        public RepositoryAsyncTests()
+        {
+            // AutoMapper
+            var autoMapperConfig = new MapperConfiguration(
+                new MapperConfigurationExpression()
+            );
 
-//            // Arrange & Act & Assert
-//            Assert.Equal(_fixture.UnitOfWork.FakeEntityRepository.ProtectedQuery(queryParameters).FirstOrDefault(), await _fixture.UnitOfWork.FakeEntityRepository.GetAsync(queryParameters));
-//        }
+            var autoMapper = autoMapperConfig.CreateMapper();
 
-//        [Theory]
-//        [MemberData(nameof(QueryParametersMemberData))]
-//        public async Task GetTModelAsync_Success(QueryParameters<FakeEntity, int> queryParameters, IQueryable<FakeEntity> queryable, IQueryable<FakeEntity> countQueryable)
-//        {
-//            ValidateQueryParametersMemberData(queryable, countQueryable);
+            // data mapper
+            var dataMapper = new FakeDataMapper(autoMapper);
 
-//            // Arrange & Act & Assert
-//            ObjectAssert.DeepEquals(_fixture.DataMapper.Map<FakeModel>(_fixture.UnitOfWork.FakeEntityRepository.ProtectedQuery(queryParameters)).FirstOrDefault(), await _fixture.UnitOfWork.FakeEntityRepository.GetAsync<FakeModel>(queryParameters));
-//        }
+            // database
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder<FakeDbContext>()
+                .UseInMemoryDatabase($"{nameof(RepositoryTests)}{Guid.NewGuid():N}");
 
-//        #endregion
+            _unitOfWork = new FakeUnitOfWork(
+                dbContext => new Repository<FakeEntity<Guid>, Guid>(dbContext, dataMapper),
+                dbContext => new Repository<FakeEntity<int>, int>(dbContext, dataMapper),
+                dbContext => new Repository<FakeEntity<string>, string>(dbContext, dataMapper),
+                dbContextOptionsBuilder.Options,
+                "dbo"
+            );
+        }
 
-//        #region GetList
+        public DbContext DbContext => _unitOfWork.ExposedDbContext;
 
-//        [Theory]
-//        [MemberData(nameof(QueryParametersMemberData))]
-//        public async Task GetListAsync_Success(QueryParameters<FakeEntity, int> queryParameters, IQueryable<FakeEntity> queryable, IQueryable<FakeEntity> countQueryable)
-//        {
-//            ValidateQueryParametersMemberData(queryable, countQueryable);
-
-//            // Arrange & Act & Assert
-//            Assert.Equal(_fixture.UnitOfWork.FakeEntityRepository.ProtectedQuery(queryParameters).ToList(), await _fixture.UnitOfWork.FakeEntityRepository.GetListAsync(queryParameters));
-//        }
-
-//        [Theory]
-//        [MemberData(nameof(QueryParametersMemberData))]
-//        public async Task GetListTModelAsync_Success(QueryParameters<FakeEntity, int> queryParameters, IQueryable<FakeEntity> queryable, IQueryable<FakeEntity> countQueryable)
-//        {
-//            ValidateQueryParametersMemberData(queryable, countQueryable);
-
-//            // Arrange & Act & Assert
-//            ObjectAssert.DeepEquals(_fixture.DataMapper.Map<FakeModel>(_fixture.UnitOfWork.FakeEntityRepository.ProtectedQuery(queryParameters)).ToList(), await _fixture.UnitOfWork.FakeEntityRepository.GetListAsync<FakeModel>(queryParameters));
-//        }
-
-//        #endregion
-
-//        #region GetPagedList
-
-//        [Theory]
-//        [MemberData(nameof(QueryParametersMemberData))]
-//        public async Task GetPagedListAsync_Success(QueryParameters<FakeEntity, int> queryParameters, IQueryable<FakeEntity> queryable, IQueryable<FakeEntity> countQueryable)
-//        {
-//            ValidateQueryParametersMemberData(queryable, countQueryable);
-
-//            if (queryParameters?.Page == null || queryParameters.Page.IsValid == false) return;
-
-//            // Arrange
-//            var pagedResultQuery = _fixture.UnitOfWork.FakeEntityRepository.ProtectedPagedResultQuery(queryParameters);
-
-//            var expectedPagedResult = new PagedResult<FakeEntity>
-//            {
-//                PageIndex = queryParameters.Page.Index,
-//                PageSize = queryParameters.Page.Size,
-//                Items = pagedResultQuery.ToList(),
-//                TotalCount = _fixture.UnitOfWork.FakeEntityRepository.Count(queryParameters)
-//            };
-
-//            // Act & Assert
-//            ObjectAssert.DeepEquals(expectedPagedResult, await _fixture.UnitOfWork.FakeEntityRepository.GetPagedListAsync(queryParameters));
-//        }
-
-//        [Theory]
-//        [MemberData(nameof(QueryParametersMemberData))]
-//        public async Task GetPagedListTModelAsync_Success(QueryParameters<FakeEntity, int> queryParameters, IQueryable<FakeEntity> queryable, IQueryable<FakeEntity> countQueryable)
-//        {
-//            ValidateQueryParametersMemberData(queryable, countQueryable);
-
-//            if (queryParameters?.Page == null || queryParameters.Page.IsValid == false) return;
-
-//            // Arrange
-//            var pagedResultQuery = _fixture.UnitOfWork.FakeEntityRepository.ProtectedPagedResultQuery<FakeModel>(queryParameters);
-
-//            var expectedPagedResult = new PagedResult<FakeModel>
-//            {
-//                PageIndex = queryParameters.Page.Index,
-//                PageSize = queryParameters.Page.Size,
-//                Items = pagedResultQuery.ToList(),
-//                TotalCount = _fixture.UnitOfWork.FakeEntityRepository.Count(queryParameters)
-//            };
-
-//            // Act & Assert
-//            ObjectAssert.DeepEquals(expectedPagedResult, await _fixture.UnitOfWork.FakeEntityRepository.GetPagedListAsync<FakeModel>(queryParameters));
-//        }
-
-//        #endregion
-
-//        #region Exists
-
-//        [Theory]
-//        [MemberData(nameof(QueryParametersMemberData))]
-//        public async Task ExistsQueryParametersAsync_Success(QueryParameters<FakeEntity, int> queryParameters, IQueryable<FakeEntity> queryable, IQueryable<FakeEntity> countQueryable)
-//        {
-//            ValidateQueryParametersMemberData(queryable, countQueryable);
-
-//            // Arrange & Act & Assert
-//            Assert.Equal(_fixture.UnitOfWork.FakeEntityRepository.ProtectedQuery(queryParameters).Any(), await _fixture.UnitOfWork.FakeEntityRepository.ExistsAsync(queryParameters));
-//        }
-
-//        #endregion
-
-//        #region Count
-
-//        [Theory]
-//        [MemberData(nameof(QueryParametersMemberData))]
-//        public async Task CountAsync_Success(QueryParameters<FakeEntity, int> queryParameters, IQueryable<FakeEntity> queryable, IQueryable<FakeEntity> countQueryable)
-//        {
-//            ValidateQueryParametersMemberData(queryable, countQueryable);
-
-//            // Arrange & Act & Assert
-//            Assert.Equal(_fixture.UnitOfWork.FakeEntityRepository.ProtectedCountQuery(queryParameters).Count(), await _fixture.UnitOfWork.FakeEntityRepository.CountAsync(queryParameters));
-//        }
-
-//        #endregion
-
-//        #region Insert
-
-//        [Fact]
-//        public async Task InsertAsync_NewEntity_Inserted()
-//        {
-//            // Arrange
-//            var dbContextOptionsBuilder = new DbContextOptionsBuilder<DbContext>().UseInMemoryDatabase(nameof(InsertAsync_NewEntity_Inserted));
-//            var fakeDbContext = new FakeDbContext(dbContextOptionsBuilder.Options, "dbo");
-//            var repository = new Repository<FakeEntity, int>(fakeDbContext, _fixture.DataMapper);
-
-//            var entity = new FakeEntity();
-
-//            // Act
-//            var insertedEntity = await repository.InsertAsync(entity);
-//            await fakeDbContext.SaveChangesAsync();
-
-//            // Assert
-//            Assert.Equal(entity.Id, insertedEntity.Id);
-//        }
-
-//        [Fact]
-//        public async Task InsertAsync_NewEntitiesList_Inserted()
-//        {
-//            // Arrange
-//            var dbContextOptionsBuilder = new DbContextOptionsBuilder<DbContext>().UseInMemoryDatabase(nameof(InsertAsync_NewEntitiesList_Inserted));
-//            var fakeDbContext = new FakeDbContext(dbContextOptionsBuilder.Options, "dbo");
-//            var repository = new Repository<FakeEntity, int>(fakeDbContext, _fixture.DataMapper);
-
-//            var entities = new List<FakeEntity>
-//            {
-//                new FakeEntity(),
-//                new FakeEntity()
-//            };
-
-//            // Act
-//            await repository.InsertAsync(entities);
-//            await fakeDbContext.SaveChangesAsync();
-
-//            // Assert
-//            Assert.Equal(await fakeDbContext.Set<FakeEntity>().CountAsync(), entities.Count);
-//        }
-
-//        #endregion
-//    }
-//}
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+    }
+}
