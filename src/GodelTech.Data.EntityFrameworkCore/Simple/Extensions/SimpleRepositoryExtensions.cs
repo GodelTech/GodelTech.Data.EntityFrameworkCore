4260ac15181bb8e3c3b6544b8238e9619525d477
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-07-29 11:13:34 +0200

    add SimpleRepository extensions delete by id

---
+++ src/GodelTech.Data.EntityFrameworkCore/Simple/Extensions/SimpleRepositoryExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace GodelTech.Data.EntityFrameworkCore.Simple
+{
+    /// <summary>
+    /// Extensions of simple repository for data layer.
+    /// </summary>
+    public static partial class SimpleRepositoryExtensions
+    {
+
+    }
+}
