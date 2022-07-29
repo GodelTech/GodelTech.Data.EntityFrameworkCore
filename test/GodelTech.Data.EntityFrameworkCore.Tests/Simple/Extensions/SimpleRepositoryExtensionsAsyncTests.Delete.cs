992e3c3305a07e838a5970682091ba8cf807f068
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-07-29 15:16:27 +0200

    add SimpleRepositoryExtensions unit tests

---
+++ test/GodelTech.Data.EntityFrameworkCore.Tests/Simple/Extensions/SimpleRepositoryExtensionsAsyncTests.Delete.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GodelTech.Data.EntityFrameworkCore.Simple;
+using Xunit;
+
+namespace GodelTech.Data.EntityFrameworkCore.Tests.Simple.Extensions
+{
+    public partial class SimpleRepositoryExtensionsAsyncTests
+    {
+        [Theory]
+        [MemberData(nameof(TypesMemberData))]
+        public async Task DeleteAsync_ByIdWhenRepositoryIsNull_ThrowsArgumentNullException<TKey>(TKey defaultKey)
+        {
+            // Arrange & Act & Assert
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+                () => SimpleRepositoryExtensions.DeleteAsync<IEntity<TKey>, TKey>(null, defaultKey)
+            );
+
+            Assert.Equal("repository", exception.ParamName);
+        }
+
+        [Theory]
+        [MemberData(nameof(TypesMemberData))]
+        public async Task DeleteAsync_ByIdsWhenRepositoryIsNull_ThrowsArgumentNullException<TKey>(TKey defaultKey)
+        {
+            // Arrange & Act & Assert
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+                () => SimpleRepositoryExtensions.DeleteAsync<IEntity<TKey>, TKey>(
+                    null,
+                    new List<TKey>
+                    {
+                        defaultKey
+                    }
+                )
+            );
+
+            Assert.Equal("repository", exception.ParamName);
+        }
+    }
+}
