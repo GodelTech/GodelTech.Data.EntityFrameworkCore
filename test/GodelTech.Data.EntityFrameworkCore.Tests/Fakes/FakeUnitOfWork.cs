6e8fbebac03f0012c944c8ff390aa85c94d42b6d
Vladimir Rodchenko <vladimir@rodchenko.by>
2023-09-19 12:22:41 +0200

    #0 improve tests

--- test/GodelTech.Data.EntityFrameworkCore.Tests/Fakes/FakeUnitOfWork.cs
+++ test/GodelTech.Data.EntityFrameworkCore.Tests/Fakes/FakeUnitOfWork.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace GodelTech.Data.EntityFrameworkCore.Tests.Fakes
 {
     public class FakeUnitOfWork : UnitOfWork<DbContext>
     {
-        public FakeUnitOfWork(IDbContextFactory<DbContext> dbContextFactory)
+        private readonly Action<bool> _onDispose;
+        private readonly Action<bool> _onAfterDispose;
+
+        public FakeUnitOfWork(
+            IDbContextFactory<DbContext> dbContextFactory,
+            Action<bool> onDispose = null,
+            Action<bool> onAfterDispose = null)
             : base(dbContextFactory)
         {
-
+            _onDispose = onDispose;
+            _onAfterDispose = onAfterDispose;
         }
 
         public void ExposedRegisterRepository<TEntity, TKey>(IRepository<TEntity, TKey> repository)
@@ -22,9 +30,20 @@ namespace GodelTech.Data.EntityFrameworkCore.Tests.Fakes
             return GetRepository<TEntity, TKey>();
         }
 
+        public bool ExposedIsDisposed => IsDisposed;
+
         public void ExposedDispose(bool disposing)
         {
             Dispose(disposing);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _onDispose?.Invoke(disposing);
+
+            base.Dispose(disposing);
+
+            _onAfterDispose?.Invoke(IsDisposed);
         }
     }
 }
