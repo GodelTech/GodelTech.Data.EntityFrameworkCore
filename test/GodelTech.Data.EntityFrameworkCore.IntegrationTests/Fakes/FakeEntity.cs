d04ff99aaf886defc67cf253146b96cf2884a2cc
Vladimir Rodchenko <vladimir@rodchenko.by>
2024-07-01 11:08:22 +0200

    #0 remove Entity to use it from GodelTech.Data

--- test/GodelTech.Data.EntityFrameworkCore.IntegrationTests/Fakes/FakeEntity.cs
+++ test/GodelTech.Data.EntityFrameworkCore.IntegrationTests/Fakes/FakeEntity.cs
@@ -1,7 +1,14 @@
-﻿namespace GodelTech.Data.EntityFrameworkCore.IntegrationTests.Fakes
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GodelTech.Data.EntityFrameworkCore.IntegrationTests.Fakes
 {
     public class FakeEntity<TKey> : Entity<TKey>
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override TKey Id { get; set; }
+
         public string Name { get; set; }
     }
 }
